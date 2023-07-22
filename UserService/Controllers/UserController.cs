a621c948f07525f3543631c3ccfdd1ed580ee16f
Piotr Zawrocki <piotrzawrocki@hotmail.com>
2023-07-22 12:21:24 +0200

    Removed ignored files

--- UserService/Controllers/UserController.cs
+++ UserService/Controllers/UserController.cs
@@ -4,16 +4,16 @@ namespace UserService.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class UserController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<UserController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public UserController(ILogger<UserController> logger)
         {
             _logger = logger;
         }
