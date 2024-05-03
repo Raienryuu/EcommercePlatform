e0b053dc99676007b9db6644f0b342b4297510c4
Peter <piotrzawrocki@hotmail.com>
2024-05-03 13:59:22 +0200

    Adjusted docker compose file for easy deployment. Added SQL script to be run on a fresh instance of SQL Server.

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -5,43 +5,56 @@ namespace Orders
 {
   public class Program
   {
-	public static void Main(string[] args)
-	{
-	  var builder = WebApplication.CreateBuilder(args);
+    public static void Main(string[] args)
+    {
+      var builder = WebApplication.CreateBuilder(args);
 
-	  // Add services to the container.
+      // Add services to the container.
 
-	  builder.Services.AddControllers();
-	  // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-	  builder.Services.AddEndpointsApiExplorer();
-	  builder.Services.AddSwaggerGen();
-	  var connectionString =
-		builder.Configuration.GetConnectionString("DefaultConnection");
-	  builder.Services.AddDbContext<OrderDbContext>(o =>
-	  {
-		o.UseSqlServer(connectionString);
-		o.AddInterceptors();
-	  });
+      builder.Services.AddControllers();
+      // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+      builder.Services.AddEndpointsApiExplorer();
+      builder.Services.AddSwaggerGen();
+    var connectionString = BuildConnectionString(builder);
+          builder.Services.AddDbContext<OrderDbContext>(o =>
+      {
+        o.UseSqlServer(connectionString);
+        o.AddInterceptors();
+      });
 
-	  MessageQueueUtils.Configure(builder.Configuration, builder.Services);
+      MessageQueueUtils.Configure(builder.Configuration, builder.Services);
 
-	  var app = builder.Build();
+      var app = builder.Build();
 
-	  // Configure the HTTP request pipeline.
-	  if (app.Environment.IsDevelopment())
-	  {
-		app.UseSwagger();
-		app.UseSwaggerUI();
-	  }
+      // Configure the HTTP request pipeline.
+      if (app.Environment.IsDevelopment())
+      {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+      }
 
-	  app.UseHttpsRedirection();
+      app.UseHttpsRedirection();
 
-	  app.UseAuthorization();
+      app.UseAuthorization();
 
 
-	  app.MapControllers();
+      app.MapControllers();
 
-	  app.Run();
-	}
+      using(var scope = app.Services.CreateAsyncScope())
+      {
+        var dbContext = scope.ServiceProvider.GetRequiredService<OrderDbContext>();
+        dbContext.Database.EnsureCreated();
+      }
+
+      app.Run();
+    }
+
+    private static string BuildConnectionString(WebApplicationBuilder builder)
+    {
+      return builder.Configuration.GetConnectionString("Host") +
+             builder.Configuration.GetConnectionString("User") +
+             builder.Configuration.GetConnectionString("Password") +
+             builder.Configuration.GetConnectionString("DefaultConnection");
+    }
   }
 }
--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -1,70 +1,83 @@
 using System.Diagnostics;
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration.Json;
 using Microsoft.OpenApi.Models;
 
 namespace ProductService
 {
-    public static class Program
+  public static class Program
   {
-	public static void Main(string[] args)
-	{
-	  var builder = WebApplication.CreateBuilder(args);
+    public static void Main(string[] args)
+    {
+      var builder = WebApplication.CreateBuilder(args);
 
 
-	  builder.Services.AddControllers();
-	  // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-	  builder.Services.AddEndpointsApiExplorer();
-	  builder.Services.AddSwaggerGen(options =>
-	  {
-		options.SwaggerDoc("v1", new OpenApiInfo
-		{
-		  Version = "v1",
-		  Title = "Products",
-		  Description = "API to manage products store in database."
-		});
+      builder.Services.AddControllers();
+      // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+      builder.Services.AddEndpointsApiExplorer();
+      builder.Services.AddSwaggerGen(options =>
+      {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Version = "v1",
+          Title = "Products",
+          Description = "API to manage products store in database."
+        });
 
-		var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-		options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-	  });
+        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+      });
 
-	  builder.Services.AddCors(o => o.AddPolicy("DevPolicy", policyBuilder =>
-	  {
-		policyBuilder.WithOrigins("http://localhost:4200")
-		  .AllowCredentials()
-		  .AllowAnyHeader();
-	  }));
+      builder.Services.AddCors(o => o.AddPolicy("DevPolicy", policyBuilder =>
+      {
+        policyBuilder.WithOrigins("http://localhost:4200")
+          .AllowCredentials()
+          .AllowAnyHeader();
+      }));
 
-	  var connectionString =
-		builder.Configuration.GetConnectionString("DefaultConnection");
+      var connectionString = BuildConnectionString(builder);
 
-	  builder.Services.AddDbContext<ProductDbContext>(options =>
-		options.UseSqlServer(connectionString)
-		  .LogTo(message => Debug.WriteLine(message))
-	  );
+      builder.Services.AddDbContext<ProductDbContext>(options =>
+        options.UseSqlServer(connectionString)
+          .LogTo(message => Debug.WriteLine(message))
+      );
 
-	  builder.Services.AddLogging();
+      builder.Services.AddLogging();
 
-	  MessageQueueUtils.Configure(builder);
+      MessageQueueUtils.Configure(builder);
 
-	  var app = builder.Build();
+      var app = builder.Build();
 
-	  if (app.Environment.IsDevelopment())
-	  {
-		app.UseSwagger();
-		app.UseSwaggerUI(options =>
-		{
-		  options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-		  options.RoutePrefix = string.Empty;
-		});
-		app.UseCors("DevPolicy");
-	  }
+      if (app.Environment.IsDevelopment())
+      {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+          options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+          options.RoutePrefix = string.Empty;
+        });
+        app.UseCors("DevPolicy");
+      }
 
+      app.MapControllers();
 
-	  app.MapControllers();
+      using(var scope = app.Services.CreateAsyncScope())
+      {
+        var dbContext = scope.ServiceProvider.GetRequiredService<ProductDbContext>();
+        dbContext.Database.EnsureCreated();
+      }
 
-	  app.Run();
-	}
+      app.Run();
+    }
+
+    private static string BuildConnectionString(WebApplicationBuilder builder)
+    {
+      return builder.Configuration.GetConnectionString("Host") +
+             builder.Configuration.GetConnectionString("User") +
+             builder.Configuration.GetConnectionString("Password") +
+             builder.Configuration.GetConnectionString("DefaultConnection");
+    }
   }
 }
