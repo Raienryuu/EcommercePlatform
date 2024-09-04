cb2e4f457f01d8eae5f0a9003e2743d89f13ce39
Peter <piotrzawrocki@hotmail.com>
2024-09-04 09:32:06 +0200

    Updated visual regression page link

--- UserClient.Tests/tests/visual-regression.spec.ts
+++ UserClient.Tests/tests/visual-regression.spec.ts
@@ -1,23 +1,23 @@
 import { test, expect } from "@playwright/test";
 
 test("regression products", async ({ page }) => {
-  await page.goto("http://localhost:4200");
+  await page.goto("https://raienryuu.github.io/EcommercePlatform/");
   await expect(page).toHaveScreenshot();
 });
 test("regression login", async ({ page }) => {
-  await page.goto("http://localhost:4200");
+  await page.goto("https://raienryuu.github.io/EcommercePlatform/");
   await expect(page).toHaveScreenshot();
 });
 test("regression register", async ({ page }) => {
-  await page.goto("http://localhost:4200");
+  await page.goto("https://raienryuu.github.io/EcommercePlatform/");
   await expect(page).toHaveScreenshot();
 });
 test("regression cart", async ({ page }) => {
-  await page.goto("http://localhost:4200");
+  await page.goto("https://raienryuu.github.io/EcommercePlatform/");
   await expect(page).toHaveScreenshot();
 });
 test("regression checkout", async ({ page }) => {
-  await page.goto("http://localhost:4200");
+  await page.goto("https://raienryuu.github.io/EcommercePlatform/");
   await expect(page).toHaveScreenshot();
 });
 
