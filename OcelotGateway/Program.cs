976a57f35d89793b0cba7694da3c68927ff4b612
Peter <piotrzawrocki@hotmail.com>
2024-10-02 13:01:03 +0200

    Ocelot configuration in docker changed to use 'http'

--- OcelotGateway/Program.cs
+++ OcelotGateway/Program.cs
@@ -1,45 +1,30 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using System.Diagnostics;
-namespace OcelotBasic
-{
-  public class Program
+
+new WebHostBuilder()
+  .UseKestrel()
+  .UseContentRoot(Directory.GetCurrentDirectory())
+  .ConfigureAppConfiguration((hostingContext, config) =>
   {
-	public static void Main(string[] args)
-	{
-	  new WebHostBuilder()
-	  .UseKestrel()
-	  .UseContentRoot(Directory.GetCurrentDirectory())
-	  .ConfigureAppConfiguration((hostingContext, config) =>
-	  {
-		IWebHostEnvironment env = hostingContext.HostingEnvironment;
-		config
-				  .AddEnvironmentVariables()
-				  .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-				  .AddJsonFile("appsettings.json", true, true)
-				  .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, false)
-				  .AddJsonFile("ocelot.json")
-				  .AddJsonFile($"ocelot.{env.EnvironmentName}.json", false, true);
-	  })
-	  .ConfigureServices((context, s) =>
-	  {
-		new Startup(context.Configuration).ConfigureServices(s);
-		s.AddOcelot();
-	  })
-	  .ConfigureLogging((hostingContext, logging) =>
-	  {
-		logging.SetMinimumLevel(LogLevel.Trace);
-		logging.AddConsole();
-	  })
-	  .Configure(app =>
-	  {
-		app.UseOcelot().Wait();
-	  })
-	  .Build()
-	  .Run();
-	}
-  }
-}
+    IWebHostEnvironment env = hostingContext.HostingEnvironment;
+    config
+      .AddEnvironmentVariables()
+      .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+      .AddJsonFile("appsettings.json", true, true)
+      .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, false)
+      .AddJsonFile("ocelot.json")
+      .AddJsonFile($"ocelot.{env.EnvironmentName}.json", false, true);
+  })
+  .ConfigureServices((context, s) =>
+  {
+    new Startup(context.Configuration).ConfigureServices(s);
+    s.AddOcelot();
+  })
+  .ConfigureLogging((hostingContext, logging) =>
+  {
+    logging.SetMinimumLevel(LogLevel.Trace);
+    logging.AddConsole();
+  })
+  .Configure(app => { app.UseOcelot().Wait(); })
+  .Build()
+  .Run();
