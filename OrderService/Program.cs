c25028f9ea7fd0701d7e74254562b704b760d8eb
Peter <piotrzawrocki@hotmail.com>
2025-01-08 13:08:44 +0100

    Optimized running docker compose for the first time
    Product service awaits for successful connection to database.
    Order service awaits for successful connection to database.

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -10,66 +10,68 @@ public class Program
 {
   public static void Main(string[] args)
   {
-	var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(args);
 
-	builder.Services.AddControllers();
-	builder.Services.AddEndpointsApiExplorer();
-	builder.Services.AddSwaggerGen();
-	var connectionString = BuildConnectionString(builder);
-	builder.Services.AddDbContext<OrderDbContext>(o =>
-	{
-	  o.UseSqlServer(connectionString);
-	});
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    var connectionString = BuildConnectionString(builder);
+    builder.Services.AddDbContext<OrderDbContext>(o =>
+    {
+      o.UseSqlServer(connectionString);
+    });
 
-	MessageQueueUtils.Configure(builder.Configuration, builder.Services);
+    MessageQueueUtils.Configure(builder.Configuration, builder.Services);
 
-	builder.Services.AddScoped<IValidator<Order>, OrderValidator>();
-	builder.Services.AddFluentValidationAutoValidation();
+    builder.Services.AddScoped<IValidator<Order>, OrderValidator>();
+    builder.Services.AddFluentValidationAutoValidation();
 
-	var app = builder.Build();
+    var app = builder.Build();
 
 
-	if (app.Environment.IsDevelopment())
-	{
-	  app.UseSwagger();
-	  app.UseSwaggerUI();
+    if (app.Environment.IsDevelopment())
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI();
 
-	  using var scope = app.Services.CreateAsyncScope();
-	  var dbContext = scope.ServiceProvider.GetRequiredService<OrderDbContext>();
+      using var scope = app.Services.CreateAsyncScope();
+      using (var dbContext = scope.ServiceProvider.GetRequiredService<OrderDbContext>())
+      {
+        CreateDevelopmentDatabase(dbContext);
+      }
+      static void CreateDevelopmentDatabase(OrderDbContext? dbContext)
+      {
+        while (true)
+        {
+          try
+          {
+            dbContext?.Database.EnsureCreated();
+          }
+          catch
+          {
+            Thread.Sleep(5000);
+            continue;
+          }
+          break;
+        }
+      }
+      app.UseHttpsRedirection();
 
-	  foreach (var x in Enumerable.Range(0, 3))
-	  {
-		CreateDevelopmentDatabase(dbContext);
-	  }
+      app.UseAuthorization();
 
-	  static void CreateDevelopmentDatabase(OrderDbContext dbContext)
-	  {
-		try
-		{
-		  dbContext.Database.EnsureCreated();
-		}
-		catch
-		{// do nothing
-		}
-	  }
-	}
+      app.MapControllers();
 
-	app.UseHttpsRedirection();
 
-	app.UseAuthorization();
 
-	app.MapControllers();
+      app.Run();
 
-	
-
-	app.Run();
+    }
   }
-
-  private static string BuildConnectionString(WebApplicationBuilder builder)
+  static string BuildConnectionString(WebApplicationBuilder builder)
   {
-	return builder.Configuration.GetConnectionString("Host") +
-		 builder.Configuration.GetConnectionString("User") +
-		 builder.Configuration.GetConnectionString("Password") +
-		 builder.Configuration.GetConnectionString("DefaultConnection");
+    return builder.Configuration.GetConnectionString("Host") +
+         builder.Configuration.GetConnectionString("User") +
+         builder.Configuration.GetConnectionString("Password") +
+         builder.Configuration.GetConnectionString("DefaultConnection");
   }
 }
--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -1,97 +1,96 @@
-using System.Diagnostics;
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
-namespace ProductService
+namespace ProductService;
+
+public class Program
 {
-  public class Program
+  public static void Main(string[] args)
   {
-	public static void Main(string[] args)
-	{
-	  var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(args);
 
 
-	  builder.Services.AddControllers();
-	  // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-	  builder.Services.AddEndpointsApiExplorer();
-	  builder.Services.AddSwaggerGen(options =>
-	  {
-		options.SwaggerDoc("v1", new OpenApiInfo
-		{
-		  Version = "v1",
-		  Title = "Products",
-		  Description = "API to manage products store."
-		});
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options =>
+    {
+      options.SwaggerDoc("v1", new OpenApiInfo
+      {
+        Version = "v1",
+        Title = "Products",
+        Description = "API to manage products store."
+      });
 
-		var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-		options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-	  });
+      var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+      options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    });
 
-	  builder.Services.AddCors(o => o.AddPolicy("DevPolicy", policyBuilder =>
-	  {
-		policyBuilder.WithOrigins("http://localhost:4200")
-		  .AllowCredentials()
-		  .AllowAnyHeader();
-	  }));
+    builder.Services.AddCors(o => o.AddPolicy("DevPolicy", policyBuilder =>
+    {
+      policyBuilder.WithOrigins("http://localhost:4200")
+      .AllowCredentials()
+      .AllowAnyHeader();
+    }));
 
-	  var connectionString = BuildConnectionString(builder);
+    var connectionString = BuildConnectionString(builder);
 
-	  builder.Services.AddDbContext<ProductDbContext>(options =>
-		options.UseSqlServer(connectionString)
-	  );
+    builder.Services.AddDbContext<ProductDbContext>(options =>
+      options.UseSqlServer(connectionString)
+    );
 
-	  builder.Services.AddLogging();
+    builder.Services.AddLogging();
 
-	  MessageQueueUtils.Configure(builder);
+    MessageQueueUtils.Configure(builder);
 
-	  var app = builder.Build();
+    var app = builder.Build();
 
-	  if (app.Environment.IsDevelopment())
-	  {
-		app.UseSwagger();
-		app.UseSwaggerUI(options =>
-		{
-		  options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-		  options.RoutePrefix = string.Empty;
-		});
-		app.UseCors("DevPolicy");
+    if (app.Environment.IsDevelopment())
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI(options =>
+      {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+      });
+      app.UseCors("DevPolicy");
 
-		using var scope = app.Services.CreateAsyncScope();
-		var dbContext = scope.ServiceProvider.GetRequiredService<ProductDbContext>();
+      using var scope = app.Services.CreateAsyncScope();
+      using (var dbContext = scope.ServiceProvider.GetRequiredService<ProductDbContext>())
+      {
+        CreateDevelopmentDatabase(dbContext);
+      }
+      static void CreateDevelopmentDatabase(ProductDbContext? dbContext)
+      {
+        while (true)
+        {
+          try
+          {
+            dbContext?.Database.EnsureCreated();
+          }
+          catch
+          {
+            Thread.Sleep(5000);
+            continue;
+          }
+          break;
+        }
+      }
+    }
 
-		foreach (var x in Enumerable.Range(0, 3))
-		{
-		  Thread.Sleep(150);
-		  CreateDevelopmentDatabase(dbContext);
-		}
-
-		static void CreateDevelopmentDatabase(ProductDbContext dbContext)
-		{
-		  try
-		  {
-			dbContext.Database.EnsureCreated();
-		  }
-		  catch
-		  {// do nothing
-		  }
-		}
-	  }
-
-	  app.MapControllers();
+    app.MapControllers();
 
 
 
-	  app.Run();
-	}
+    app.Run();
+  }
 
-	private static string BuildConnectionString(WebApplicationBuilder builder)
-	{
-	  return builder.Configuration.GetConnectionString("Host") +
-			 builder.Configuration.GetConnectionString("User") +
-			 builder.Configuration.GetConnectionString("Password") +
-			 builder.Configuration.GetConnectionString("DefaultConnection");
-	}
+  private static string BuildConnectionString(WebApplicationBuilder builder)
+  {
+    return builder.Configuration.GetConnectionString("Host") +
+           builder.Configuration.GetConnectionString("User") +
+           builder.Configuration.GetConnectionString("Password") +
+           builder.Configuration.GetConnectionString("DefaultConnection");
   }
 }
