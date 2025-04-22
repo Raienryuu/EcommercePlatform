47ca71afb9a15a1623d83a0eff8387c95f5f36b7
Peter <piotrzawrocki@hotmail.com>
2025-04-22 11:04:48 +0200

    Added customer information to set delivery method request

--- UserClient/src/app/models/customer-address.model.ts
+++ UserClient/src/app/models/customer-address.model.ts
@@ -1,5 +1,3 @@
-
-
 export interface CustomerAddress {
   Id: number;
   FullName: string;
--- UserClient/src/app/models/delivery-method.model.ts
+++ UserClient/src/app/models/delivery-method.model.ts
@@ -1,7 +1,10 @@
+import { CustomerAddress } from './customer-address.model';
+
 export interface DeliveryMethod {
   deliveryId: string;
   name: string;
   deliveryType: 'DeliveryPoint' | 'DirectCustomerAddress';
   paymentType: 'Cash' | 'Online';
   price: number;
+  customerInformation: CustomerAddress | null;
 }
