be43e63b6628d0bea750a4446f05535f28ff5569
Peter <piotrzawrocki@hotmail.com>
2025-05-22 12:23:55 +0200

    Removed redundant functionality from OrderStatus
    OrderStatus is not inside of static class now

--- OrderService/Models/OrderStatus.cs
+++ OrderService/Models/OrderStatus.cs
@@ -1,26 +1,10 @@
 namespace OrderService.Models;
 
-public static class OrderStatus
+public enum OrderStatus
 {
-  public enum Type
-  {
-    AwaitingConfirmation,
-    Confirmed,
-    ReadyToShip,
-    Shipped,
-    Cancelled,
-  }
-
-  public static Type FromString(string status)
-  {
-    return status switch
-    {
-      nameof(Type.AwaitingConfirmation) => Type.AwaitingConfirmation,
-      nameof(Type.Confirmed) => Type.Confirmed,
-      nameof(Type.ReadyToShip) => Type.ReadyToShip,
-      nameof(Type.Shipped) => Type.Shipped,
-      nameof(Type.Cancelled) => Type.Cancelled,
-      _ => throw new NotImplementedException("Invalid OrderStatus string given."),
-    };
-  }
+  AwaitingConfirmation,
+  Confirmed,
+  ReadyToShip,
+  Shipped,
+  Cancelled,
 }
