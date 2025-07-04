8a368471549ab4e1d61dc6d05095ad51291afe01
Peter <piotrzawrocki@hotmail.com>
2025-07-04 13:10:30 +0200

    Added nongeneric ServiceResult

--- Common/ServiceResult.cs
+++ Common/ServiceResult.cs
@@ -7,6 +7,13 @@ public abstract partial record ServiceResult<T>(
   string? ErrorMessage
 );
 
+public abstract partial record ServiceResult(bool IsSuccess, int StatusCode, string? ErrorMessage);
+
+public partial record ErrorServiceResult(int StatusCode, string ErrorMessage)
+  : ServiceResult(false, StatusCode, ErrorMessage);
+
+public partial record SuccessServiceResult(int StatusCode) : ServiceResult(true, StatusCode, null);
+
 public partial record ErrorServiceResult<T>(int StatusCode, string ErrorMessage)
   : ServiceResult<T>(default, false, StatusCode, ErrorMessage);
 
@@ -22,6 +29,10 @@ public static partial class ServiceResults
   public static ErrorServiceResult<T> Error<T>(string errorMessage, int statusCode) =>
     new(statusCode, errorMessage);
 
-  public static ErrorServiceResult<T> NotFound<T>(string errorMessage) =>
-    new(404, errorMessage);
+  public static ErrorServiceResult<T> NotFound<T>(string errorMessage) => new(404, errorMessage);
+
+  public static SuccessServiceResult Success(int statusCode) => new(statusCode);
+
+  public static ErrorServiceResult Error(string errorMessage, int statusCode) =>
+    new(statusCode, errorMessage);
 }
