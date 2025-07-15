bbe0300edaf386d38d42f9a0067fe524ca4fb38b
Peter <piotrzawrocki@hotmail.com>
2025-07-15 09:31:59 +0200

    Added cancellation token for IProductCategoryService

--- ProductService/Services/IProductCategoryService.cs
+++ ProductService/Services/IProductCategoryService.cs
@@ -5,10 +5,25 @@ namespace ProductService.Services;
 
 public interface IProductCategoryService
 {
-  Task<ServiceResult<List<ProductCategory>>> GetProductCategories();
-  Task<ServiceResult<ProductCategory>> GetProductCategory(int id);
-  Task<ServiceResult<List<ProductCategory>>> GetChildrenCategories(int id);
-  Task<ServiceResult> UpdateProductCategory(int id, ProductCategory productCategory);
-  Task<ServiceResult<ProductCategory>> CreateProductCategory(ProductCategory productCategory);
-  Task<ServiceResult> DeleteProductCategory(int id);
+  Task<ServiceResult<List<ProductCategory>>> GetProductCategories(
+    CancellationToken cancellationToken = default
+  );
+  Task<ServiceResult<ProductCategory>> GetProductCategory(
+    int id,
+    CancellationToken cancellationToken = default
+  );
+  Task<ServiceResult<List<ProductCategory>>> GetChildrenCategories(
+    int id,
+    CancellationToken cancellationToken = default
+  );
+  Task<ServiceResult> UpdateProductCategory(
+    int id,
+    ProductCategory productCategory,
+    CancellationToken cancellationToken = default
+  );
+  Task<ServiceResult<ProductCategory>> CreateProductCategory(
+    ProductCategory productCategory,
+    CancellationToken cancellationToken = default
+  );
+  Task<ServiceResult> DeleteProductCategory(int id, CancellationToken cancellationToken = default);
 }
--- ProductService/Services/ProductCategoryService.cs
+++ ProductService/Services/ProductCategoryService.cs
@@ -8,15 +8,18 @@ public class ProductCategoryService(ProductDbContext context) : IProductCategory
 {
   private readonly ProductDbContext _context = context;
 
-  public async Task<ServiceResult<List<ProductCategory>>> GetProductCategories()
+  public async Task<ServiceResult<List<ProductCategory>>> GetProductCategories(CancellationToken ct)
   {
-    var categories = await _context.ProductCategories.ToListAsync();
+    var categories = await _context.ProductCategories.AsNoTracking().ToListAsync(cancellationToken: ct);
     return ServiceResults.Ok(categories);
   }
 
-  public async Task<ServiceResult<ProductCategory>> GetProductCategory(int id)
+  public async Task<ServiceResult<ProductCategory>> GetProductCategory(int id, CancellationToken ct)
   {
-    var productCategory = await _context.ProductCategories.FindAsync(id);
+    var productCategory = await _context
+      .ProductCategories.AsNoTracking()
+      .Where(x => x.Id == id)
+      .FirstOrDefaultAsync(cancellationToken: ct);
 
     if (productCategory == null)
     {
@@ -26,9 +29,12 @@ public class ProductCategoryService(ProductDbContext context) : IProductCategory
     return ServiceResults.Ok(productCategory);
   }
 
-  public async Task<ServiceResult<List<ProductCategory>>> GetChildrenCategories(int id)
+  public async Task<ServiceResult<List<ProductCategory>>> GetChildrenCategories(int id, CancellationToken ct)
   {
-    var productCategory = await _context.ProductCategories.Where(x => x.Id == id).FirstOrDefaultAsync();
+    var productCategory = await _context
+      .ProductCategories.AsNoTracking()
+      .Where(x => x.Id == id)
+      .FirstOrDefaultAsync(cancellationToken: ct);
 
     if (productCategory == null)
     {
@@ -38,24 +44,28 @@ public class ProductCategoryService(ProductDbContext context) : IProductCategory
     var releatedChildCategories = await _context
       .ProductCategories.AsNoTracking()
       .Where(x => x.ParentCategory != null && x.ParentCategory.Id == productCategory.Id)
-      .ToListAsync();
+      .ToListAsync(cancellationToken: ct);
 
     return ServiceResults.Ok(releatedChildCategories);
   }
 
-  public async Task<ServiceResult> UpdateProductCategory(int id, ProductCategory productCategory)
+  public async Task<ServiceResult> UpdateProductCategory(
+    int id,
+    ProductCategory productCategory,
+    CancellationToken ct
+  )
   {
     if (id != productCategory.Id)
     {
       return ServiceResults.Error("IDs do not match.", 400);
     }
 
-    if (!await ProductCategoryExists(id))
+    if (!await ProductCategoryExists(id, ct))
     {
       return ServiceResults.Error("No category found with a given ID.", 404);
     }
 
-    if (!await AssignParent(productCategory))
+    if (!await AssignParent(productCategory, ct))
     {
       return ServiceResults.Error("Parent category not found.", 404);
     }
@@ -63,7 +73,7 @@ public class ProductCategoryService(ProductDbContext context) : IProductCategory
     try
     {
       _context.Entry(productCategory).State = EntityState.Modified;
-      await _context.SaveChangesAsync();
+      await _context.SaveChangesAsync(ct);
     }
     catch (DbUpdateConcurrencyException)
     {
@@ -73,54 +83,58 @@ public class ProductCategoryService(ProductDbContext context) : IProductCategory
     return ServiceResults.Success(204);
   }
 
-  public async Task<ServiceResult<ProductCategory>> CreateProductCategory(ProductCategory productCategory)
+  public async Task<ServiceResult<ProductCategory>> CreateProductCategory(
+    ProductCategory productCategory,
+    CancellationToken ct
+  )
   {
     var existingCategory = await _context
       .ProductCategories.Where(_ => _.CategoryName == productCategory.CategoryName)
-      .FirstOrDefaultAsync();
+      .FirstOrDefaultAsync(cancellationToken: ct);
 
     if (existingCategory is not null)
     {
       return ServiceResults.Error<ProductCategory>("Category already exists.", 409); // Conflict
     }
 
-    if (!await AssignParent(productCategory))
+    if (!await AssignParent(productCategory, ct))
     {
       return ServiceResults.Error<ProductCategory>("Parent category not found.", 400); // Bad Request
     }
 
     _context.ProductCategories.Add(productCategory);
-    await _context.SaveChangesAsync();
+    await _context.SaveChangesAsync(ct);
 
     return ServiceResults.Success(productCategory, 201); // Created
   }
 
-  public async Task<ServiceResult> DeleteProductCategory(int id)
+  public async Task<ServiceResult> DeleteProductCategory(int id, CancellationToken ct)
   {
-    var productCategory = await _context.ProductCategories.FindAsync(id);
+    var productCategory = await _context.ProductCategories.FindAsync([id], cancellationToken: ct);
     if (productCategory == null)
     {
       return ServiceResults.Error("No category found with a given ID.", 404);
     }
 
     _context.ProductCategories.Remove(productCategory);
-    await _context.SaveChangesAsync();
+    await _context.SaveChangesAsync(ct);
 
     return ServiceResults.Success(204); // No Content
   }
 
-  private Task<bool> ProductCategoryExists(int id)
+  private Task<bool> ProductCategoryExists(int id, CancellationToken ct)
   {
-    return _context.ProductCategories.AnyAsync(e => e.Id == id);
+    return _context.ProductCategories.AnyAsync(e => e.Id == id, cancellationToken: ct);
   }
 
-  private async Task<bool> AssignParent(ProductCategory productCategory)
+  private async Task<bool> AssignParent(ProductCategory productCategory, CancellationToken ct)
   {
     if (productCategory.ParentCategory is null)
       return true;
 
-    var parent = await _context.ProductCategories.FirstOrDefaultAsync(x =>
-      x.Id == productCategory.ParentCategory.Id
+    var parent = await _context.ProductCategories.FirstOrDefaultAsync(
+      x => x.Id == productCategory.ParentCategory.Id,
+      cancellationToken: ct
     );
 
     if (parent?.CategoryName == productCategory.ParentCategory.CategoryName)
