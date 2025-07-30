b46f8a14094ba8d37ae486981f0a3273eccce085
Peter <piotrzawrocki@hotmail.com>
2025-07-30 10:28:11 +0200

    Use route level code splitting

--- UserClient/src/app/app-routing.module.ts
+++ UserClient/src/app/app-routing.module.ts
@@ -1,12 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { ProductsComponent } from './components/catalog/catalog.component';
-import { CartComponent } from './components/cart/cart.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { OrderDetailsComponent } from './components/order-details/order-details.component';
 
 const routes: Routes = [
   {
@@ -14,13 +7,53 @@ const routes: Routes = [
     pathMatch: 'full',
     redirectTo: 'login',
   },
-  { path: 'products', component: ProductsComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'cart', component: CartComponent },
-  { path: 'checkout/:orderId', component: CheckoutComponent },
-  { path: 'details/:id', component: ProductDetailsComponent },
-  { path: 'myorder/:id', component: OrderDetailsComponent },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./components/catalog/catalog.component').then(
+        (m) => m.ProductsComponent,
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/login/login.component').then(
+        (m) => m.LoginComponent,
+      ),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./components/register/register.component').then(
+        (m) => m.RegisterComponent,
+      ),
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./components/cart/cart.component').then((m) => m.CartComponent),
+  },
+  {
+    path: 'checkout/:orderId',
+    loadComponent: () =>
+      import('./components/checkout/checkout.component').then(
+        (m) => m.CheckoutComponent,
+      ),
+  },
+  {
+    path: 'details/:id',
+    loadComponent: () =>
+      import('./components/product-details/product-details.component').then(
+        (m) => m.ProductDetailsComponent,
+      ),
+  },
+  {
+    path: 'myorder/:id',
+    loadComponent: () =>
+      import('./components/order-details/order-details.component').then(
+        (m) => m.OrderDetailsComponent,
+      ),
+  },
 ];
 
 @NgModule({
