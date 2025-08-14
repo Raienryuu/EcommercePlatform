76ce87cb00b1d4bcba0ea7f1219c46ec5d188bcc
Peter <piotrzawrocki@hotmail.com>
2025-08-14 08:37:57 +0200

    Fix test RegisterNewUser_ValidData_ReturnsSuccess

--- Tests/IdentityService.Tests/Unit/UserServiceTests.cs
+++ Tests/IdentityService.Tests/Unit/UserServiceTests.cs
@@ -28,19 +28,27 @@ public class UserServiceTests
   {
     var connection = new SqliteConnection("datasource=:memory:");
     connection.Open();
-    var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-        .UseSqlite(connection)
-        .Options;
+    var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlite(connection).Options;
     _dbContext = new ApplicationDbContextFake(options);
     _dbContext.Database.EnsureCreated();
     _userManagerMock = new Mock<UserManager<IdentityUser<Guid>>>(
-        Mock.Of<IUserStore<IdentityUser<Guid>>>(), null!, null!, null!, null!, null!, null!, null!, null!);
+      Mock.Of<IUserStore<IdentityUser<Guid>>>(),
+      null!,
+      null!,
+      null!,
+      null!,
+      null!,
+      null!,
+      null!,
+      null!
+    );
     _configuration = new ConfigurationBuilder()
-      .AddInMemoryCollection(new Dictionary<string, string?>
+      .AddInMemoryCollection(
+        new Dictionary<string, string?>
         {
-          {"Jwt:Key", "testkeytestkeytestkeytestkeytestkey12"},
-          {"Jwt:Issuer", "testissuer"},
-          {"Jwt:ExpireTimeInDays", "1"}
+          { "Jwt:Key", "testkeytestkeytestkeytestkeytestkey12" },
+          { "Jwt:Issuer", "testissuer" },
+          { "Jwt:ExpireTimeInDays", "1" },
         }
       )
       .Build();
@@ -53,15 +61,19 @@ public class UserServiceTests
   public async Task RegisterNewUser_ValidData_ReturnsSuccess()
   {
     var newUser = SampleUserData.NewUser;
-    _userManagerMock.Setup(m => m.CreateAsync(It.IsAny<IdentityUser<Guid>>()))
-        .ReturnsAsync(IdentityResult.Success)
-        .Callback<IdentityUser<Guid>>((user) =>
+    _userManagerMock
+      .Setup(m => m.CreateAsync(It.IsAny<IdentityUser<Guid>>()))
+      .ReturnsAsync(IdentityResult.Success)
+      .Callback<IdentityUser<Guid>>(
+        (user) =>
         {
           _dbContext.Users.Add(user);
           _dbContext.SaveChanges();
-        });
-    _userManagerMock.Setup(m => m.AddToRoleAsync(It.IsAny<IdentityUser<Guid>>(), It.IsAny<string>()))
-        .ReturnsAsync(IdentityResult.Success);
+        }
+      );
+    _userManagerMock
+      .Setup(m => m.AddToRoleAsync(It.IsAny<IdentityUser<Guid>>(), It.IsAny<string>()))
+      .ReturnsAsync(IdentityResult.Success);
 
     var result = await _userService.RegisterNewUser(newUser, CancellationToken.None);
 
@@ -90,69 +102,74 @@ public class UserServiceTests
     var result = await _userService.GetUsernameForLoggedUser(user.Id, CancellationToken.None);
 
     Assert.True(result.IsSuccess);
-    Assert.Equal(user.UserName, result.Value);
+    Assert.Equal(user.UserName, result.Value, true);
   }
 
   [Fact]
   public async Task RegisterNewUser_CreateAsyncFails_ReturnsError()
   {
-      var newUser = SampleUserData.NewUser;
-      var identityError = new IdentityError { Description = "Create failed" };
-      var failedResult = IdentityResult.Failed(identityError);
-      _userManagerMock.Setup(m => m.CreateAsync(It.IsAny<IdentityUser<Guid>>()))
-          .ReturnsAsync(failedResult);
-      var result = await _userService.RegisterNewUser(newUser, CancellationToken.None);
-      Assert.False(result.IsSuccess);
-      Assert.Contains("Create failed", result.ErrorMessage);
+    var newUser = SampleUserData.NewUser;
+    var identityError = new IdentityError { Description = "Create failed" };
+    var failedResult = IdentityResult.Failed(identityError);
+    _userManagerMock.Setup(m => m.CreateAsync(It.IsAny<IdentityUser<Guid>>())).ReturnsAsync(failedResult);
+    var result = await _userService.RegisterNewUser(newUser, CancellationToken.None);
+    Assert.False(result.IsSuccess);
+    Assert.Contains("Create failed", result.ErrorMessage);
   }
 
   [Fact]
   public async Task RegisterNewUser_AddToRoleFails_ReturnsError()
   {
-      var newUser = SampleUserData.NewUser;
-      _userManagerMock.Setup(m => m.CreateAsync(It.IsAny<IdentityUser<Guid>>()))
-          .ReturnsAsync(IdentityResult.Success)
-          .Callback<IdentityUser<Guid>>((user) => {
-              _dbContext.Users.Add(user);
-              _dbContext.SaveChanges();
-          });
-      var identityError = new IdentityError { Description = "Role failed" };
-      var failedResult = IdentityResult.Failed(identityError);
-      _userManagerMock.Setup(m => m.AddToRoleAsync(It.IsAny<IdentityUser<Guid>>(), It.IsAny<string>()))
-          .ReturnsAsync(failedResult);
-      var result = await _userService.RegisterNewUser(newUser, CancellationToken.None);
-      Assert.False(result.IsSuccess);
-      Assert.Contains("Role failed", result.ErrorMessage);
+    var newUser = SampleUserData.NewUser;
+    _userManagerMock
+      .Setup(m => m.CreateAsync(It.IsAny<IdentityUser<Guid>>()))
+      .ReturnsAsync(IdentityResult.Success)
+      .Callback<IdentityUser<Guid>>(
+        (user) =>
+        {
+          _dbContext.Users.Add(user);
+          _dbContext.SaveChanges();
+        }
+      );
+    var identityError = new IdentityError { Description = "Role failed" };
+    var failedResult = IdentityResult.Failed(identityError);
+    _userManagerMock
+      .Setup(m => m.AddToRoleAsync(It.IsAny<IdentityUser<Guid>>(), It.IsAny<string>()))
+      .ReturnsAsync(failedResult);
+    var result = await _userService.RegisterNewUser(newUser, CancellationToken.None);
+    Assert.False(result.IsSuccess);
+    Assert.Contains("Role failed", result.ErrorMessage);
   }
 
   [Fact]
   public async Task LogInUser_UserNotFound_ReturnsError()
   {
-      var credentials = SampleUserData.UserCredentials;
-      _userManagerMock.Setup(m => m.FindByNameAsync(credentials.Login)).ReturnsAsync((IdentityUser<Guid>?)null);
-      var result = await _userService.LogInUser(credentials, CancellationToken.None);
-      Assert.False(result.IsSuccess);
-      Assert.Contains("Not able to get matching values", result.ErrorMessage);
+    var credentials = SampleUserData.UserCredentials;
+    _userManagerMock.Setup(m => m.FindByNameAsync(credentials.Login)).ReturnsAsync((IdentityUser<Guid>?)null);
+    var result = await _userService.LogInUser(credentials, CancellationToken.None);
+    Assert.False(result.IsSuccess);
+    Assert.Contains("Not able to get matching values", result.ErrorMessage);
   }
 
   [Fact]
   public async Task LogInUser_InvalidPassword_ReturnsError()
   {
-      var credentials = SampleUserData.UserCredentials;
-      var user = new IdentityUser<Guid> { Id = Guid.NewGuid(), UserName = credentials.Login };
-      _userManagerMock.Setup(m => m.FindByNameAsync(credentials.Login)).ReturnsAsync(user);
-      _userManagerMock.Setup(m => m.CheckPasswordAsync(user, credentials.Password)).ReturnsAsync(false);
-      var result = await _userService.LogInUser(credentials, CancellationToken.None);
-      Assert.False(result.IsSuccess);
-      Assert.Contains("Not able to get matching values", result.ErrorMessage);
+    var credentials = SampleUserData.UserCredentials;
+    var user = new IdentityUser<Guid> { Id = Guid.NewGuid(), UserName = credentials.Login };
+    _userManagerMock.Setup(m => m.FindByNameAsync(credentials.Login)).ReturnsAsync(user);
+    _userManagerMock.Setup(m => m.CheckPasswordAsync(user, credentials.Password)).ReturnsAsync(false);
+    var result = await _userService.LogInUser(credentials, CancellationToken.None);
+    Assert.False(result.IsSuccess);
+    Assert.Contains("Not able to get matching values", result.ErrorMessage);
   }
 
   [Fact]
   public async Task GetUsernameForLoggedUser_UserNotFound_ReturnsError()
   {
-      var nonExistentUserId = Guid.NewGuid();
-      var result = await _userService.GetUsernameForLoggedUser(nonExistentUserId, CancellationToken.None);
-      Assert.False(result.IsSuccess);
-      Assert.Contains("Couldn't find user with given Id", result.ErrorMessage);
+    var nonExistentUserId = Guid.NewGuid();
+    var result = await _userService.GetUsernameForLoggedUser(nonExistentUserId, CancellationToken.None);
+    Assert.False(result.IsSuccess);
+    Assert.Contains("Couldn't find user with given Id", result.ErrorMessage);
   }
 }
+
